352f1161022155dfc1fadc0ee8b2512e77bb0333
Abdullah Zahid <azravian@gmail.com>
2021-06-10 19:29:53 +0300

    removed API keys

--- Test.py
+++ Test.py
@@ -4,8 +4,8 @@ import datetime
 
 
 # Original Echange Api
-api_key = "ZV4QxddoSR6HkzQYrLIWOlp0BeJIapHrIuYnCt5Z2rsD98YLp8dhuO8Icb05pvew"
-api_secret = "hWFfWYzB1FzSPzjvUSNN0g1Mw3vsK66Lq6tQlePy7fad7w7OozWLkPQYLPxwTmUP"
+api_key = ""
+api_secret = ""
 
 
 client = Client(api_key, api_secret)
