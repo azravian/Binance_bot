72f347f6d3faa6e04265537894cf07b8bcc649f6
Abdullah Zahid <azravian@gmail.com>
2021-06-13 12:18:15 +0300

    Added Comments

--- Buyer.py
+++ Buyer.py
@@ -7,38 +7,40 @@ api_key = ""
 api_secret = ""
 '''
 
-# Original Echange Api
+# Original Exchange Api
+
 api_key = ""
 api_secret = ""
 
 client = Client(api_key, api_secret)
 
-Symbol = "ETHUSDT"
+Symbol = "ETHUSDT" # Coin name with USDT 
 BuyAmount = 12 # in USDT
-Type = "market"
-Side = "buy"
+Type = "market" # The order will be in market price
+Side = "buy" # Buy Order
 
-FinishTask = False        
-Now = datetime.now()
-LaunchTime = Now.replace(hour = 19, 
-                         minute = 8, 
+FinishTask = False # Flag variable for loop continuation  
+Now = datetime.now() # Current date and time 
+LaunchTime = Now.replace(hour = 19, # Launch date and time, suggested to keep 2 second less the actual launch time. 
+                         minute = 8, # if launching at 16:00:00, set the time as 15:59:58
                          second = 58,
                          microsecond = 0)
-while True:
-    Now = datetime.now()
-    if Now > LaunchTime:
-        while True:
+while True: # Main loop, basically for time checking.
+    Now = datetime.now() #Fetch the time continuously.
+        if Now > LaunchTime: # when time is greater the set time.
+        while True: # Second loop to send the order command concurrently
+        print("Sending order at:", date.now())
             try:
-                order = client.create_order(symbol = Symbol, side = Side, type = Type, quoteOrderQty=BuyAmount)
-            except BinanceAPIException as e:
-                print("Error: ", e.message, datetime.now())
+                order = client.create_order(symbol = Symbol, side = Side, type = Type, quoteOrderQty=BuyAmount) # Order command
+            except BinanceAPIException as e: # To handle exception and keeping code from crashing
+                print("Error: ", e.message, datetime.now()) # prints the error message 
             else:
-                print(order["fills"])
+                print(order["fills"]) # The dteails of successful order
                 print("Now: ", Now, "Pre: ", LaunchTime)
-                OrderTime = datetime.fromtimestamp(int(order["transactTime"])/1000)
+                OrderTime = datetime.fromtimestamp(int(order["transactTime"])/1000) # Time of Order execution
                 print("Time:", OrderTime)
-                print("Time Difference: ", OrderTime - Now)
-                FinishTask = True
+                print("Time Difference: ", OrderTime - Now) # The difference of sending order and execution of order.
+                FinishTask = True # to break out of the main loop.
                 break
     if FinishTask:
         break
